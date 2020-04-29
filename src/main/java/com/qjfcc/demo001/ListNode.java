14ade7fed74e4e637a78a2c94d712ce6c8ac59e0
长风dad <mengzhixing999@163.com>
2020-04-29 15:39:59 +0800

    commit this version

--- src/main/java/com/qjfcc/demo001/ListNode.java
+++ src/main/java/com/qjfcc/demo001/ListNode.java
@@ -5,7 +5,7 @@ package com.qjfcc.demo001;
  * @AddTime 2019/11/29 17:21
  */
 public class ListNode {
-      public int val;
-      public ListNode next;
-      public ListNode(int x) { val = x; }
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
 }
